eb6be75360c26a670905558ba578362328e66875
MarkyDDev <makdanieldev@gmail.com>
2023-07-13 21:17:27 +0200

    Hero_Update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+//variables
 const btnHamburger = document.querySelector("#btnHamburger");
 const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
+
 
 btnHamburger.addEventListener("click", function () {
   console.log("click hamburger");
